7bfe017ca69f86adef7a40a04cd4b389fcb63351
Flohhhhh <48927090+Flohhhhh@users.noreply.github.com>
2023-03-07 07:09:49 +0000

    Initial

---
+++ real-brake-lights/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "real-brake-lights"
+description "A simple resource that enables brake lights when the vehicle is stopped"
+author "Floh"
+version "1.0.0"
+
+client_scripts {
+	'main.lua'
+}
---
+++ real-brake-lights/main.lua
@@ -0,0 +1,16 @@
+-- brake lights will turn on below this speed in MPH
+local threshold = 3
+
+CreateThread(function()
+    while true do Wait(250)
+        local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+        if not vehicle then goto continue end
+
+        local speed = GetEntitySpeed(vehicle) * 2.236936
+        if speed <= threshold then
+            SetVehicleBrakeLights(vehicle, true)
+        end
+
+        ::continue::
+    end
+end)
