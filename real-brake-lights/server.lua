778fa51ed2f3158722cc669cd668f8dc76025023
Floh <48927090+Flohhhhh@users.noreply.github.com>
2024-12-05 09:16:02 -0500

    Update server.lua

--- real-brake-lights/server.lua
+++ real-brake-lights/server.lua
@@ -19,18 +19,3 @@ AddEventHandler("rbl:setParked", function(netId, state)
   local vehicle = NetworkGetEntityFromNetworkId(netId)
   Entity(vehicle).state.rbl_parked = state
 end)
-
---[[
-
-options
-# option 1
-- have another state on vehicle for whether it's in parked state
-- set parked state after x time with randomness
-- when in parked state, set brake lights to false
-- getting out of vehicle sets parked state to true
-  - this could possible ovverride the current system for disabling when there is no driver
-  - or maybe old method can be adapted to trigger this?
-
-# option 2
-- 
-]]
