bd7ae035bbc3a3e7096dbb336abc4fdbd2bcd29e
FaisalAlY_ <faisal.ahmed.alyahya@gmail.com>
2026-02-04 06:48:12 +0400

    Modify park effect timers and enhance comments
    Updated park timer values and added comments for clarity.

--- real-brake-lights/config.lua
+++ real-brake-lights/config.lua
@@ -1,13 +1,24 @@
 Config = {
-  -- the park effect will turn off the brake lights after a random amount of time in seconds between parkTimerMin and parkTimerMax
-  -- this is to simulate the vehicle being put in park after some time of being stopped
+  --[[
+    the park effect will turn off the brake lights after a random amount of time in seconds between parkTimerMin and parkTimerMax
+    this is to simulate the vehicle being put in park after some time of being stopped
+  ]]
   enableParkEffect = true,
-  parkTimerMin = 15, -- must be less than parkTimerMax (default 15)
-  parkTimerMax = 60, -- must be greater than parkTimerMin (default 60)
+  parkTimerMin = 30, -- must be less than parkTimerMax (default 15)
+  parkTimerMax = 90, -- must be greater than parkTimerMin (default 60)
 
-  -- this is the speed at which the brake lights will turn off in MPH
-  -- good values are beteween 3 - 20
-  -- lower values will make the brake lights more responsive
-  -- higher values can make a more realistic effect and reduce gap between braking and stopping
+  --[[
+    this is the speed at which the brake lights will turn off in MPH
+    good values are beteween 3 - 20
+    lower values will make the brake lights more responsive
+    higher values can make a more realistic effect and reduce gap between braking and stopping
+  ]]
   brakeLightThreshold = 8, -- default 8
+  blackOutCommand = 'blackout', -- nil or '' to remove command
+
+  -- Turn and Hazard signals
+  enableSignals = true, -- enable or disable turn and hazard signals
+  leftSignalKey = 'LEFT', -- use LEFT for left arrow key
+  rightSignalKey = 'RIGHT', -- use RIGHT for right arrow key
+  hazardSignalKey = 'DOWN', -- use DOWN for down arrow key
 }
